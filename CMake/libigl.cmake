474891119779ebe1bc815d535b8df665875303ba
Jimmy Envall <jimmy.envall@inf.ethz.ch>
2024-03-22 09:36:43 +0100

    Update CMakeLists

--- CMake/libigl.cmake
+++ CMake/libigl.cmake
@@ -1,8 +1,8 @@
-set(FETCHCONTENT_SOURCE_DIR_LIBIGL "${CMAKE_CURRENT_SOURCE_DIR}/../libigl")
 include(FetchContent)
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG v2.5.0
+    GIT_TAG v2.4.0
 )
 FetchContent_MakeAvailable(libigl)
+set(FETCHCONTENT_SOURCE_DIR_LIBIGL "${CMAKE_CURRENT_BINARY_DIR}/_deps/libigl")
